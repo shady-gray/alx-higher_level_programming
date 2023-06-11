46036bd3377a0d2c0172fe3f5b2384b98f506b9f
Basil Bassey <akpanbasilbassey@gmail.com>
2023-06-11 16:55:43 +0100

    added task 6, 7,  9

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(format='%(message)s')
+    log = logging.getLogger(__name__)
+    log.warning('#pythoniscool')

360c1c3d7056c541e0fe6141c117c6668f2d7083
Basil <akpanbasilbassey@gmail.com>
2023-06-11 03:37:05 -0700

    Change argument to arguments in the third elif block

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         for i in range(1, len(sys.argv)):
             print("{:d}: {:s}".format(i, sys.argv[i]))
     elif len(sys.argv) > 2:
-        print("{:d} argument:".format(len(sys.argv) - 1))
+        print("{:d} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{:d}: {:s}".format(i, sys.argv[i]))

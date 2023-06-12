b19a6cabf700418f87b1f55febfba6383935b5f5
Basil <akpanbasilbassey@gmail.com>
2023-06-11 20:21:35 -0700

    Making slight changes in task 7 file

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import logging
-    logging.basicConfig(format='%(message)s')
-    log = logging.getLogger(__name__)
-    log.warning('#pythoniscool')
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))

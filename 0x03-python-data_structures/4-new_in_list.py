0cca6e5dafb422c952e33413db27d644688efb51
Basil Bassey <akpanbasilbassey@gmail.com>
2023-06-13 05:03:25 +0100

    Adding Task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my_listCpy = my_list[0:len(my_list)]
+    if idx < 0:
+        return my_listCpy
+    elif idx > len(my_list):
+        return my_listCpy
+    else:
+        my_listCpy[idx] = element
+        return my_listCpy

14acedab30a300ec13fe1d6a01f96f56de014259
Basil <akpanbasilbassey@gmail.com>
2023-06-13 21:40:36 -0700

    Corrected index error in file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     my_listCpy = my_list[0:len(my_list)]
     if idx < 0:
         return my_listCpy
-    elif idx > len(my_list):
+    elif idx > len(my_list) - 1:
         return my_listCpy
     else:
         my_listCpy[idx] = element

975752f10d9b548f8ad0df9e7751600361ebdb73
Basil <akpanbasilbassey@gmail.com>
2023-06-14 14:08:10 -0700

    Solving logic error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    arg_count = len(sys.argv) -1
+    arg_count = len(sys.argv) - 1
     if arg_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     n1 = int(sys.argv[1])
     n2 = int(sys.argv[3])
-    functions =[add, sub, mul, div]
+    functions = [add, sub, mul, div]
     for func, op in enumerate(operators):
         if sys.argv[2] == op:
             print("{} {} {} = {}".format(n1, op, n2, functions[func](n1, n2)))

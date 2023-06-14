36b8996a38ac22f5da983db177b217f07573ff2f
Basil Bassey <akpanbasilbassey@gmail.com>
2023-06-14 08:26:07 +0100

    clearing logic error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
     import sys
-    arg_count = len(sys.argv) - 1
+    arg_count = len(sys.argv) -1
     if arg_count != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    elif arg_count == 3:
-        n1 = int(sys.argv[1])
-        n2 = int(sys.argv[3])
-        if (sys.argv[2]
-                != "+" and sys.argv[2]
-                != "-" and sys.argv[2]
-                != "*" and sys.argv[2]
-                != "/"):
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-        elif sys.argv[2] == "+":
-            print("{} {} {} = {}\
-                ".format(n1, sys.argv[2], n2, calculator_1.add(n1, n2)))
-        elif sys.argv[2] == "-":
-            print("{} {} {} = {}\
-                ".format(n1, sys.argv[2], n2, calculator_1.sub(n1, n2)))
-        elif sys.argv[2] == "*":
-            print("{} {} {} = {}\
-                ".format(n1, sys.argv[2], n2, calculator_1.mul(n1, n2)))
-        elif sys.argv[2] == "/":
-            print("{} {} {} = {}\
-                ".format(n1, sys.argv[2], n2, calculator_1.div(n1, n2)))
+
+    operators = ['+', '-', '*', '/']
+    if sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    n1 = int(sys.argv[1])
+    n2 = int(sys.argv[3])
+    functions =[add, sub, mul, div]
+    for func, op in enumerate(operators):
+        if sys.argv[2] == op:
+            print("{} {} {} = {}".format(n1, op, n2, functions[func](n1, n2)))
+            break
+    sys.exit(0)

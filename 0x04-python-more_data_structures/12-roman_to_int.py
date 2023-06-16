56e0a2a623af6271da41d238a003424764741a8a
Basil Bassey <akpanbasilbassey@gmail.com>
2023-06-16 01:48:33 +0100

    Addng task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-'''
-    Goal
-    - a function that converts Roman Numerals to an integer
-    
-    Algorithm
-    - Create a Dictionary of the core roman numeral figures and link {R_dict}
-      their keys (Roman numeral) to values (the corresponding integer)
-    - split input string into single characters
-    - store individual characters in a list [chr_list]
-    - create a variable for the storing the result integer(final_int)
-    - loop tru the list, assigning a as index for R_dict (R_dict[a]) and incrementing final_int 
-    - return final_int
-'''
 from functools import reduce
 def roman_to_int(roman_string):
     if roman_string == None:

06cfd3c61123806ecc9ba4b410adc9f1d602ac9b
Basil Bassey <akpanbasilbassey@gmail.com>
2023-06-29 03:42:42 +0100

    Adding Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module: 1-square
+Return: definition of square
+"""
+
+
+class Square:
+    """this is a class for a Square"""
+    def __init__(self, size):
+        """initializes a square instance
+        Args:
+            size (int): the size of the square
+            self (string): the self argument
+        """
+        self.__size = size

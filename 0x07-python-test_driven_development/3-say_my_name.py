7bec25b359d09cde27c12ff6dad13acda8c9cea9
Basil Bassey <akpanbasilbassey@gmail.com>
2023-07-16 03:50:36 +0100

    debugging

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,51 @@
 #!/usr/bin/python3
-"""
-This is the "3-say_my-name" module.
-The 3-say_my_name  module supplies one function, say_my_name.
+""""
+
+This module contains a function that prints a name
+
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints "My name is" followed by the first name and optional last name"""
-    if type(first_name) is not str:
+    '''This function prints name (<first name> <last name>)
+
+    Args:
+        first_name (str): The fisrt name to be printed
+        last_name (str): The last name to be printed
+
+    Raises:
+        TypeError: If either the first_name and last_name are not strings
+
+    '''
+
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
+
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is", first_name, last_name)
+    print(f"My name is {first_name} {last_name}")#!/usr/bin/python3
+""""
+
+This module contains a function that prints a name
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    '''This function prints name (<first name> <last name>)
+
+    Args:
+        first_name (str): The fisrt name to be printed
+        last_name (str): The last name to be printed
+
+    Raises:
+        TypeError: If either the first_name and last_name are not strings
+
+    '''
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
